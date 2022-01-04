66331d2a67f3f7ca761f2986a3e2adb2e474d08d
Raclett3 <im@s283.net>
2022-01-05 03:02:01 +0900

    Wait for child processes

--- src/bin/minesweeper/main.rs
+++ src/bin/minesweeper/main.rs
@@ -1,30 +1,38 @@
 mod game;
 
 use game::MineSweeper;
-use nix::unistd::{fork, ForkResult};
+use nix::{
+    sys::wait::wait,
+    unistd::{fork, ForkResult},
+};
 use std::io::{stdin, stdout};
 use std::net::TcpListener;
 use tui::runner::{run_game_on_telnet, run_game_on_tty};
+
+fn listen_on_tcp(ipaddr: &str) -> std::io::Result<()> {
+    std::thread::spawn(|| loop {
+        let _ = wait();
+    });
+
+    let listener = TcpListener::bind(ipaddr)?;
+    for stream in listener.incoming().flatten() {
+        if let Ok(ForkResult::Child) = unsafe { fork() } {
+            let read_stream = stream.try_clone()?;
+            let write_stream = stream;
+            read_stream.set_read_timeout(Some(std::time::Duration::from_secs(300)))?;
+
+            let _ = run_game_on_telnet(MineSweeper::new(0), read_stream, write_stream);
+            return Ok(());
+        }
+    }
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     let args: Vec<_> = std::env::args().take(3).collect();
     let args_str: Vec<_> = args.iter().map(|x| x.as_str()).collect();
     match args_str.as_slice() {
-        [_, "--telnet", ipaddr] => {
-            let listener = TcpListener::bind(ipaddr)?;
-            for stream in listener.incoming().flatten() {
-                if let Ok(ForkResult::Child) = unsafe { fork() } {
-                    let read_stream = stream.try_clone()?;
-                    let write_stream = stream;
-                    read_stream.set_read_timeout(Some(std::time::Duration::from_secs(300)))?;
-
-                    let _ = run_game_on_telnet(MineSweeper::new(0), read_stream, write_stream);
-                    return Ok(());
-                }
-            }
-
-            Ok(())
-        }
+        [_, "--telnet", ipaddr] => listen_on_tcp(ipaddr),
         _ => run_game_on_tty(MineSweeper::new(0), stdin(), stdout()),
     }
 }
